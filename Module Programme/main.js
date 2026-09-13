3d26b96fb1dce1cef97707c3657982de5fce80da
Soumodeep Mondal <mondalsoumodeep815@gmail.com>
2026-09-13 13:40:01 +0530

    Rename Bio to Biod and update console output

--- Module Programme/main.js
+++ Module Programme/main.js
@@ -1,5 +1,5 @@
-const {Bio}= require ("./module1.js");
+const {Biod}= require ("./module1.js");
 const {BioData}=require("./module2.js");
 
 console.log(Bio,BioData);
-console.table({Bio,BioData});
+console.table({Biod,BioData});
