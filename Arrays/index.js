5ea8a8667e943cda97c2f7522e558e53635f58ad
soumodeep815-tech <mondalsoumodeep815@gmail.com>
2026-09-14 19:21:50 +0530

    Add in js some arrays by soumo

---
+++ Arrays/index.js
@@ -0,0 +1,34 @@
+
+//array is a collection of data. it is hetrogenoues(mixed)
+
+let arr=["Soumodeep","Sneha","Sandhya","Madhusudan"]//it is an Array (Approach1)
+ console.log(arr);
+
+ let arr1=new Array("Ranit","Shatakshi","Nisha")//Approach2 
+ console.log(arr1);
+
+
+ let array=[
+    1,false,true,"Soumo",'deep'//it is mixed array
+ ]
+ console.log(array);
+
+ let arr2=["Amaar","Apple",'banana',array]
+ console.log(arr2);
+
+ //Arrays alwys start with 0 index
+ console.log(arr[3]); //it is accessing
+
+
+ arr[3]="Home" //updating
+ console.log(arr);
+
+ let removed=arr2.pop();
+ console.log(arr2);
+ 
+ 
+ 
+ 
+ 
+ 
+ 
